63280ce73bee0a1163cfc328a53d4c89c137a888
Mateusz Mindewicz <mindewiczm@gmail.com>
2023-09-10 16:39:30 +0200

    Added updating functionality and fixed modal

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let inputEmail = document.querySelector(".user-email");
 let addBtn = document.querySelector(".add-user");
 let submitBtn = document.querySelector(".submit");
 let closeBtn = document.querySelector(".close");
-let updateBtn = document.querySelector(".update");
+let updateBtn = document.querySelectorAll(".update");
 let deleteBtn = document.querySelector(".delete");
 
 // blocks
@@ -24,6 +24,8 @@ let section = document.querySelector(".management");
 
 let trInfo = document.querySelector(".user-info");
 let tBody = document.querySelector("#table-body");
+
+let menuTitle = document.querySelector(".menu-title");
 
 //forms
 
@@ -42,9 +44,24 @@ function toggleModal() {
   section.classList.toggle("is-blurred");
 }
 
+function changeMenuToSubmit() {
+  menuTitle.innerHTML = "Add user";
+  submitBtn.classList.remove("update-btn");
+  submitBtn.classList.add("submit");
+  submitBtn.innerHTML = "Submit";
+}
+
+function changeMenuToUpdate() {
+  menuTitle.innerHTML = "Update User";
+  submitBtn.classList.remove("submit");
+  submitBtn.classList.add("update-btn");
+  submitBtn.innerHTML = "Update";
+}
+
 //Handling buttons
 
 addBtn.addEventListener("click", (e) => {
+  changeMenuToSubmit();
   toggleModal();
 });
 
@@ -52,13 +69,42 @@ closeBtn.addEventListener("click", () => {
   toggleModal();
 });
 
-submitBtn.addEventListener("click", () => {
-  trClone = trInfo.cloneNode(true);
-  let children = trClone.childNodes;
-  children[1].innerText = formFirstName.value;
-  children[3].innerText = formSecondName.value;
-  children[5].innerText = formEmail.value;
-  children[7].innerText = formPhone.value;
+menuAdd.addEventListener("click", (e) => {
+  //Adding new user to table
+  if (e.target.classList.contains("submit")) {
+    trClone = trInfo.cloneNode(true);
+    let children = trClone.childNodes;
+    children[1].innerText = formFirstName.value;
+    children[3].innerText = formSecondName.value;
+    children[5].innerText = formEmail.value;
+    children[7].innerText = formPhone.value;
+    tBody.appendChild(trClone);
+    changeMenuToSubmit();
+    toggleModal();
+  }
+});
 
-  tBody.append(trClone);
+//Updating (need to listen on main, due to dynamic creating of users)
+main.addEventListener("click", (e) => {
+  if (e.target.classList.contains("update")) {
+    let parent = e.target.closest("tr");
+    let children = parent.childNodes;
+    formFirstName.value = children[1].innerText;
+    formSecondName.value = children[3].innerText;
+    formEmail.value = children[5].innerText;
+    formPhone.value = children[7].innerText;
+
+    //new values from form
+
+    changeMenuToUpdate();
+    toggleModal();
+    menuAdd.addEventListener("click", (e) => {
+      if (e.target.classList.contains("update-btn")) {
+        children[1].innerText = formFirstName.value;
+        children[3].innerHTML = formSecondName.value;
+        children[5].innerHTML = formEmail.value;
+        children[7].innerHTML = formPhone.value;
+      }
+    });
+  }
 });
